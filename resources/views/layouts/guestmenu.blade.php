4727f168330a774b4866e04c50f5beedebd57bea
Steven Straker <s.strakers@utoronto.ca>
2022-10-03 16:51:58 -0400

    Add route for event image data

--- resources/views/layouts/guestmenu.blade.php
+++ resources/views/layouts/guestmenu.blade.php
@@ -3,9 +3,10 @@
     <li><a href="{{ url('/guest/story') }}">Story</a></li>
     <li><a href="{{ url('/guest/proposal') }}">Proposal</a></li>
     <li><a href="{{ url('/guest/recap') }}">Recap</a></li>
-    {{-- <li><a href="{{ url('/guest/rsvp') }}">RSVP</a></li>--}}
+    {{-- <li><a href="{{ url('/guest/rsvp') }}">RSVP</a></li> --}}
     <li><a href="{{ url('/guest/venue') }}">Venue</a></li>
     <li><a href="{{ url('/guest/gallery') }}">Gallery</a></li>
+    <li><a href="{{ url('/guest/events') }}">Events</a></li>
     <li><a href="{{ url('/guest/thanks') }}">From Us to You</a></li>
     <li><a href="{{ url('/guest/logout') }}">Logout</a></li>
 </ul>
