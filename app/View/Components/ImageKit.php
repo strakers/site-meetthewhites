00837874a4f19232e8a4b132b935856288bdcc01
Steven Straker <s.strakers@utoronto.ca>
2022-10-04 18:39:35 -0400

    Create component to load stored imagekit data

--- app/View/Components/ImageKit.php
+++ app/View/Components/ImageKit.php
@@ -3,18 +3,39 @@
 namespace App\View\Components;
 
 use Illuminate\View\Component;
+use Storage;
+use ErrorException;
 
 class ImageKit extends Component
 {
     public $images = [];
+    public $json = '';
+
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct($images)
+    public function __construct($src)
     {
-        $this->images = $images ?? [];
+        if(preg_match("/[^a-zA-Z0-9\.\-_]+/", $src))
+            throw new ErrorException('Invalid file name');
+
+        $json = Storage::disk('imagekit')->get($src);
+        $json = trim($json);
+
+        if(! $json)
+            throw new ErrorException('JSON file is empty');
+
+        $this->json = $json;
+        $data = json_decode($json);
+        if(! $data)
+            throw new ErrorException('Invalid JSON found in file');
+
+        if(! isset($data->result) || ! is_array($data->result))
+            throw new ErrorException('JSON file is missing valid result');
+
+        $this->images = $data->result;
     }
 
     /**
