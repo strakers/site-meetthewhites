5691ea16d6cc2e50f18acaf4770ebe5e36b65742
Steven Straker <s.strakers@utoronto.ca>
2022-10-04 18:39:13 -0400

    Access imagekit stored files easily

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,11 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'imagekit' => [
+            'driver' => 'local',
+            'root' => storage_path('app/imagekit'),
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
